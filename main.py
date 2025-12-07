dfa9d10b10ac920cee1e572bff748f18049dcd31
Bruno Vilela Batista <brunovilela2002@hotmail.com>
2025-12-07 18:30:06 -0300

    Add functionality to fetch my steam wishlist and their actual price from steam API.

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import streamlit as st
 from data import load_price_data
 from charts import plot_matplotlib, plot_altair, plot_highcharts
+from steam import *
 
 # Load data from SQLite database
 df = load_price_data()
 df = df.sort_values(by="Date").reset_index(drop=True)
 
 # Create tabs
-tabs = st.tabs(["Gráficos", "Dados"])
+tabs = st.tabs(["Gráficos", "Dados", "SteamData", "WishList"])
 with tabs[0]:
     st.title("Comparativo de Gráficos — Matplotlib / Altair / Highcharts")
     st.write("Dados: Histórico de preços – Horizon Forbidden West")
@@ -33,3 +34,11 @@ with tabs[0]:
 with tabs[1]:
     st.title("Dados")
     st.dataframe(df, use_container_width=True)
+
+with tabs[2]:
+    st.title("SteamData")
+    getSteamWishList()
+
+with tabs[3]:
+    st.title("WishList")
+    viewWishlistDatabase()
